1e254e1a1d0558b06ba24d1b2b945703f87f7c1d
Kamal.A_SYDANI <kamalaminu94@gmail.com>
2025-08-12 10:30:06 +0100

    change status color

--- src/components/PartnerInsContainer.vue
+++ src/components/PartnerInsContainer.vue
@@ -50,21 +50,25 @@
                 </div>
                 <div class="self-center flex gap-3">
                   <div>
-                    <svg
+                    <!-- <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
                       class="size-6"
-                      :class="getStatusColor(spt.status)"
+                      
                     >
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
                       />
-                    </svg>
+                    </svg> -->
+                    <div
+                      class="h-3 w-3 rounded-full mt-2"
+                      :class="getStatusColor(spt.Status_of_support)"
+                    ></div>
                   </div>
 
                   <p>{{ statusNameChange(spt.Status_of_support) }}</p>
@@ -150,12 +154,14 @@ const expand = (key) => {
 
 const getStatusColor = (status) => {
   let cls = "";
-  if (status == "Ongoing") {
-    cls = "text-rsmp-sec";
+  if (status == "In Progress") {
+    cls = "bg-yellow-400";
   } else if (status == "Completed") {
-    cls = "text-success";
+    cls = "bg-green-400";
+  } else if (status == "Pending") {
+    cls = "bg-red-400";
   }
-
+  console.log(status, cls);
   return cls;
 };
 
